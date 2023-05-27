ed5e826c4e9e4be16891f72727e9136caeaec103
xKusabimaru <khalidsa890@gmail.com>
2023-05-27 03:46:43 +0300

    home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,30 @@
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="home">
+      <div className="home-students">
+        <img src="home-students.svg" alt="students"></img>
+        <p className="home-name">Students</p>
+        <p className="home-info">243</p>
+      </div>
+      <div className="home-course">
+        <img src="home-course.svg" alt="course"></img>
+        <p className="home-name">Course</p>
+        <p className="home-info">13</p>
+      </div>
+      <div className="home-payments">
+        <img src="home-payments.svg" alt="payments"></img>
+        <p className="home-name">Payments</p>
+        <p className="home-info">
+          <span>INR </span>556,000
+        </p>
+      </div>
+      <div className="home-users">
+        <img src="home-users.svg" alt="users"></img>
+        <p className="home-name home-user-name">Users</p>
+        <p className="home-info">3</p>
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
