580f004eb1dee8853cea777088852796bc0233f6
xKusabimaru <khalidsa890@gmail.com>
2023-05-29 02:21:22 +0300

    ignoring env

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,13 +3,13 @@ import { collection, getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.VITE_APIKEY,
-  authDomain: process.env.VITE_AUTH,
-  projectId: process.env.VITE_ID,
-  storageBucket: process.env.VITE_STORAGE,
-  messagingSenderId: process.env.VITE_SENDER,
-  appId: process.env.VITE_APP,
-  measurementId: process.env.VITE_MEASUREMENT
+  apiKey: "AIzaSyC_eQ17EpAI4q9QkTRgVgIH45s_iR5IhWM",
+  authDomain: "react-internship-task-381a2.firebaseapp.com",
+  projectId: "react-internship-task-381a2",
+  storageBucket: "react-internship-task-381a2.appspot.com",
+  messagingSenderId: "444759951644",
+  appId: "1:444759951644:web:01ee22c5ae9a9f126252d9",
+  measurementId: "G-Z3SEB322RE"
 };
 
 const app = initializeApp(firebaseConfig);
